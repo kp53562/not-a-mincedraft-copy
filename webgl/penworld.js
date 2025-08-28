469516cadb22cee0cf62bbc2d9b1665491a57bbc
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-08-27 18:14:26 -0700

    fixing world gen for the third time because I broke it

--- webgl/penworld.js
+++ webgl/penworld.js
@@ -432,7 +432,7 @@ textureAtlasPromise.then(createTexture)
     textureAtlas = texture
 
     // TEMP "world gen"
-    for (let y = 0; y < 10; y++) {
+    for (let y = 0; y < 8; y++) {
       for (let x = 0; x < CHUNK_SIZE; x++) {
         for (let z = 0; z < CHUNK_SIZE; z++) {
           subchunk.setBlock(new Vector3(x, y, z), new Block(y==0 ? 'cobblestone' : y < 4 ? 'stone' : y < 7 ? 'dirt' : 'grass'))
