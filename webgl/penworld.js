d6af64bc5aee2e4ac1191153a23130751ee61af3
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-09-03 12:39:59 -0700

    more jump power to compensate for gravity changes

--- webgl/penworld.js
+++ webgl/penworld.js
@@ -545,7 +545,7 @@ function render () {
       velocity.y -= elapsedTime * speed
     }
     if (keys[' '] && fallingTime<0.5) {
-      velocity.y += elapsedTime * (speed^2)
+      velocity.y += elapsedTime * (speed^3)
     }
     velocity.y += (-0.9*(fallingTime**2));
     const movement = new Vector2()
